06084f8fa6d33835024b417e9fcdbbace3338f67
Colin Pringle-Wood <colin.pringle-wood@aussiecommerce.com.au>
2019-12-09 10:59:01 +1100

    trying to sort issues

--- index.js
+++ index.js
@@ -2,49 +2,49 @@
     Platform.Load("core", "1");
 
 try {
-    var upsertRow = function(api, localSchedule) {
-      var updateObject = {
-          CustomerKey: 'DE_Example',
-          Properties: [
-              {
-                  Name: 'id',
-                  Value: localSchedule.id
-              },
-              {
-                  Name: 'region',
-                  Value: localSchedule.region
-              },
-              {
-                  Name: 'ModifiedDate',
-                  Value: Platform.Function.Now()
-              },
-              {
-                  Name: 'brand',
-                  Value: localSchedule.brand
-              },
-              {
-                  Name: 'start',
-                  Value: localSchedule.start
-              },
-              {
-                  Name: 'end',
-                  Value: localSchedule.end
-              },
-              {
-                  Name: 'type',
-                  Value: localSchedule.type
-              },
-              {
-                  Name: 'offer_id_salesforce_external',
-                  Value: localSchedule.offer_id_salesforce_external
-              }
-          ]
-      };
-      Write(Stringify(updateObject))
-      var options = {SaveOptions: [{'PropertyName': '*', SaveAction: 'UpdateAdd'}]};
-      var res = api.updateItem('DataExtensionObject', updateObject, options);
-      Write(Stringify(res))
-    }
+    //var upsertRow = function(api, localSchedule) {
+    //  var updateObject = {
+    //      CustomerKey: 'DE_Example',
+    //      Properties: [
+    //          {
+    //              Name: 'id',
+    //              Value: localSchedule.id
+    //          },
+    //          {
+    //              Name: 'region',
+    //              Value: localSchedule.region
+    //          },
+    //          {
+    //              Name: 'ModifiedDate',
+    //              Value: Platform.Function.Now()
+    //          },
+    //          {
+    //              Name: 'brand',
+    //              Value: localSchedule.brand
+    //          },
+    //          {
+    //              Name: 'start',
+    //              Value: localSchedule.start
+    //          },
+    //          {
+    //              Name: 'end',
+    //              Value: localSchedule.end
+    //          },
+    //          {
+    //              Name: 'type',
+    //              Value: localSchedule.type
+    //          },
+    //          {
+    //              Name: 'offer_id_salesforce_external',
+    //              Value: localSchedule.offer_id_salesforce_external
+    //          }
+    //      ]
+    //  };
+    //  Write(Stringify(updateObject))
+    //  var options = {SaveOptions: [{'PropertyName': '*', SaveAction: 'UpdateAdd'}]};
+    //  var res = api.updateItem('DataExtensionObject', updateObject, options);
+    //  Write(Stringify(res))
+    //}
 
     var response = HTTP.Get('https://api.luxgroup.com/api/public-offers?limit=200');
     Write(response.Status + '<br />');
@@ -61,8 +61,9 @@ try {
     for (var i = 0, len = schedules.length; i < len; i++) {
       var schedule = schedules[i]
       var localSchedule = schedule[region]
+      Write(Stringify(localSchedule)
 
-      upsertRow(api, localSchedule)
+      //upsertRow(api, localSchedule)
     }
 } catch(err) {
   var message  =  "an error occured " + err.message
