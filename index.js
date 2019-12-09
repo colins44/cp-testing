075d67f9def89431f45bf862b871d8937d813a1c
Colin Pringle-Wood <colin.pringle-wood@aussiecommerce.com.au>
2019-12-09 11:07:36 +1100

    got to stringify

--- index.js
+++ index.js
@@ -56,7 +56,7 @@
       var result = results[i]
       schedules.push(result.list_visibility_schedules)
     }
-    Write(schedules)
+    Write(Stringify(schedules))
     //var api = new Script.Util.WSProxy();
     //for (var i = 0, len = schedules.length; i < len; i++) {
     //  var schedule = schedules[i]
