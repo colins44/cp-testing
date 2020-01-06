d8e57d5eb26387e4fc7d54749d969a747adf8e98
Colin Pringle-Wood <colin.pringle-wood@aussiecommerce.com.au>
2020-01-06 15:00:02 +1100

    logging data

--- index.js
+++ index.js
@@ -65,20 +65,20 @@
    }
 
 
-   var frontPageDE = DataExtension.Init("E8C0D2E0-F86F-4E87-8AC4-DB8E6AF39A5D")
-   var schedules = []
-   for (var i = 0, len = results.length; i < len; i++) {
-     var result = results[i]
-     schedules.push(result.list_visibility_schedules)
-   }
-   Write('<br />');
-   var api = new Script.Util.WSProxy();
-   for (var i = 0, len = schedules.length; i < len; i++) {
-     var schedule = schedules[i]
-     var localSchedule = schedule[region]
-     Write('<br />');
-     upsertRow(api, localSchedule)
-   }
+   //var frontPageDE = DataExtension.Init("E8C0D2E0-F86F-4E87-8AC4-DB8E6AF39A5D")
+   //var schedules = []
+   //for (var i = 0, len = results.length; i < len; i++) {
+   //  var result = results[i]
+   //  schedules.push(result.list_visibility_schedules)
+   //}
+   //Write('<br />');
+   //var api = new Script.Util.WSProxy();
+   //for (var i = 0, len = schedules.length; i < len; i++) {
+   //  var schedule = schedules[i]
+   //  var localSchedule = schedule[region]
+   //  Write('<br />');
+   //  upsertRow(api, localSchedule)
+   //}
 
 </script>
 
