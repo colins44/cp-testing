4c68c9f64798c00d92b92288733e74db54eba8a3
Colin Pringle-Wood <colin.pringle-wood@aussiecommerce.com.au>
2020-01-06 11:09:06 +1100

    pagenations

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 <script runat=server>
     Platform.Load("core", "1");
-    var region = "SG"
+    var region = "AU"
+    var limit = 200
+    var resultsCount = 0
+    var results = []
+    var page = 1
+    var url = 'https://api.luxgroup.com/api/public-offers?limit=' + limit 
 
     var upsertRow = function(api, localSchedule) {
       var updateObject = {
@@ -46,11 +51,20 @@
       var res = api.updateItem('DataExtensionObject', updateObject, options);
     }
 
-    var response = HTTP.Get('https://api.luxgroup.com/api/public-offers?limit=200');
+    while (resultCount < limit) {
+      url = url  + '&page=' + page
+      var response = HTTP.Get(url);
+      var data = Platform.Function.ParseJSON(response.Content)
+      results = results.concat(data.results)
+      page = ++page
+      resultCount = resultCount + data.results.length
+      if (data.results.length === 0) {
+        break;
+      }
+    }
+
     Write(response.Status + '<br />');
     var frontPageDE = DataExtension.Init("E8C0D2E0-F86F-4E87-8AC4-DB8E6AF39A5D")
-    var data = Platform.Function.ParseJSON(response.Content)
-    var results = data.result
     var schedules = []
     for (var i = 0, len = results.length; i < len; i++) {
       var result = results[i]
@@ -62,7 +76,7 @@
       var schedule = schedules[i]
       var localSchedule = schedule[region]
       Write('<br />');
-
-     upsertRow(api, localSchedule)
+      upsertRow(api, localSchedule)
     }
 </script>
+
