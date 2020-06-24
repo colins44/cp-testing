6b2d6ffa8b39f6a0a7073d54ccbc89b9f45b9f84
colin pringlewood <colin.pringlewood@gmail.com>
2020-06-24 16:08:47 +1000

    fixing imports

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 <script runat=server>
+  // page can be found at https://cloud.em.luxuryescapes.com/cp_test
+  Platform.Load("Core", "1.1.5");
   Platform.Function.ContentBlockByKey('ssjs-lib');
 
-  debugMode = ['console'];
-
+  var debugMode = ['console'];
   debug('we are testing here');
-  
+
 </script>
 
