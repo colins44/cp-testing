5a5c69549c31b69279baf0e096f15250482bcec7
colin pringlewood <colin.pringlewood@gmail.com>
2020-11-30 11:23:14 +1100

    adding sha function

--- index.js
+++ index.js
@@ -3,6 +3,28 @@
   Platform.Load("Core", "1");
   Platform.Function.ContentBlockByKey('ssjs-lib');
 
+  function SHA256(string,encoding) {
+    var varName = '@amp__SHA256';
+  
+    // AMP decleration
+    var amp = "\%\%[ ";
+    // function open
+    amp += "set "+varName+" = SHA256(";
+    // parameters
+    amp += "'" + string + "'";
+    if(encoding) {
+        amp += "'" + encoding + "'";
+    }
+    // function close
+    amp += ") ";
+    // output
+    amp += "output(concat("+varName+")) ";
+    // end of AMP
+    amp += "]\%\%";
+  
+    return Platform.Function.TreatAsContent(amp);
+  }
+
   var debugMode = ['console'];
   debug('we are testing here');
   console_log('is the console.log working')
